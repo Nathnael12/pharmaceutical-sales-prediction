d91b81a45e925ede1fb1409fbc8c6032314825f0
Natty <natnaelmasresha@gmail.com>
2022-09-06 09:02:04 +0300

    add csv readers

---
+++ scripts/util.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import dvc.api as dvc
+
+class Utiil:
+    
+    def read_from_file(self,path):
+        """
+            Load data from a csv file
+        """
+        try:
+            df = pd.read_csv(path)
+        except FileNotFoundError:
+            print("File not found.")
+        return df
+    def read_from_dvc(self,path,repo,rev):
+        """
+            Load data from a dvc storage
+        """
+        try:
+            data_url = dvc.get_url(path=path,repo=repo,rev=rev)
+            df =pd.read_csv(data_url)
+        except:
+            print("Something went wrong!")
+        return df
